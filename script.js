c3e05158169bf0d4b6f81a68c995873852bb1571
Ngea Harry Kum <ngea0001@algonquinlive.com>
2022-05-11 18:28:29 -0400

    Test export

---
+++ script.js
@@ -0,0 +1,13 @@
+// Data types
+
+const iAmBool = true;
+const iAmString = "string";
+const iAmNull = null;
+
+
+
+const pet = 'dogs';
+const iAmNumber = 3;
+console.loh(`I have ${iAmNumber} is an
+employee at uOttawa.
+${employeeName3} has ${iAmNumber} ${pet}`);
